83a4defc1af768f06ced97feea28a4e095feb212
Allef Sousa <allefsousa_1@hotmail.com>
2020-01-19 17:17:26 -0300

    Criando a estrutura do banco de dados

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,10 @@ const {Router} = require('express');
 
 const routes  = Router();
 
-routes.post('/users', (req,resp) =>{
+routes.post('/devs', (req,resp) =>{
     console.log(req.body);
+    const {github_username} = req.body;  // recuprando nome passado no body
     return resp.json({message:'Pelo amor de cristo'});
-})
+});
 
 module.exports = routes;
