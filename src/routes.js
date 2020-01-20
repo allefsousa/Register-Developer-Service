e374e6cc4f11763b572861e221d617c807d4c160
Allef Sousa <allefsousa_1@hotmail.com>
2020-01-19 22:52:15 -0300

    Salvando o desenvolvedor no mongoDB

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,30 @@
 const { Router } = require('express');
 const axios = require('axios');
+const Dev = require('./models/dev');
 
 const routes  = Router();
 
 routes.post('/devs', async (req,resp) =>{
     console.log(req.body);
-    const {github_username} = req.body;  // recuprando nome passado no body
+    const {github_username, techs } = req.body;  // recuprando nome passado no body
+    console.log(github_username)
 
-    const r = await axios.get(`https://api.github.com/users/${github_username}`)
-    console.log(r.data)
-    return resp.json({message:'Pelo amor de cristo'});
+
+    const response = await axios.get(`https://api.github.com/users/${github_username}`)
+    console.log(response.data)
+
+    const {name = login,avatar_url,bio} = response.data
+
+    const techArrays = techs.split(',').map(tech => tech.trim())
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs:techArrays,
+    });
+
+    return resp.json(dev);
 });
 
 module.exports = routes;
